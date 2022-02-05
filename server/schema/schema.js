2d1c19a7362303e2ff1546a0c0b460c26af39747
Animesh Dwivedi <animeshdwivedinwm@gmail.com>
2022-02-05 08:30:25 -0600

    adding dummy data and graphiql view for the query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,9 @@
 const graphql = require('graphql');
-
+const _ = require('lodash');
 const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+//Dummy data
+const books = [{name: 'Book 1', id : '1'},{name: 'Book 2', id : '2'},{name: 'Book 3', id : '3'}]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -16,8 +19,9 @@ const RootQuery =  new GraphQLObjectType({
         book: { 
             type: BookType, 
             args: {id: {type: GraphQLString}},
-            resolve(parent, args) {                
-                //code to get data from database/datasource
+            resolve(parent, args) {       
+                //code to get data from database/datasource         
+                _.find(books,{id: args.id})
             }
         }
     }
