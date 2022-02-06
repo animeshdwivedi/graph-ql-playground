2c9323afaa2d390a219d2be46caf689cae942efe
Animesh Dwivedi <animeshdwivedinwm@gmail.com>
2022-02-05 19:51:19 -0600

    updating with realistic data and adding a relation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,15 +8,32 @@ const {
 } = graphql;
 
 //Dummy data
-const books = [{name: 'Book 1', id : '1'},{name: 'Book 2', id : '2'},{name: 'Book 3', id : '3'}]
-const authors = [{name: 'Author 1', id : '1'},{name: 'Author 2', id : '2'},{name: 'Author 3', id : '3'}]
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+const authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
 
 //Book GraphQL Object
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         name: {type: GraphQLString},
-        id: {type: GraphQLID},        
+        id: {type: GraphQLID},
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return(_.find(authors, {id: parent.authorId}))
+            }
+        }        
     })
 });
 
